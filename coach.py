f2a35fcbecdb3b18764e3066af01ea07b3f62146
SebbieMzingKe <sebbievilar2@gmail.com>
2024-12-07 13:40:27 +0300

    Initial commit

---
+++ coach.py
@@ -0,0 +1,15 @@
+# user_age, user_name = 30, "Sebbie"
+# # print(user_age, user_name)
+
+# has_high_income = True
+# has_good_credit = True
+
+
+# if has_high_income and has_good_credit:
+#     print("Eligible for a loan")
+# else:
+#     print("not eligible
+
+# month = ['jan', 'feb', 'mar']
+
+# print(month.get['jan'])
