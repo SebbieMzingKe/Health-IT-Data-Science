1bb8185c696b957b8ec2b8bdfe38df991c4c17cc
SebbieMzingKe <sebbievilar2@gmail.com>
2024-12-14 22:11:38 +0300

    update (core): add files

--- coach.py
+++ coach.py
@@ -8,8 +8,30 @@
 # if has_high_income and has_good_credit:
 #     print("Eligible for a loan")
 # else:
-#     print("not eligible
+#     print("not eligible")
 
 # month = ['jan', 'feb', 'mar']
 
 # print(month.get['jan'])
+
+# customer = {
+#     "name": "Seb",
+#     "age": 30,
+#     "verified": True
+# }
+
+# print(customer.get("birth", "jan 2, 2004"))
+
+# customer["name"] = "Mzing"
+
+# print(customer)
+
+
+
+# formatting strings
+
+age = 30,
+lecturer = "Raf"
+school = "MMUST"
+
+print("I am " + str(age) + "and my lecturer is " + lecturer + "I study at "+ school)
